c70843cbf7e4780d19363e10261a2ca90d7f2feb
zmeyer44 <meyerz@wustl.edu>
2020-12-20 15:34:28 -0500

    Added Stripe integration and updated favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51I0XwFDGGC9cQGRMgAtwPD5m7k9w4pI0e37lFKWu5lSTxxhODJsGX7bmkIk3tjc4vkRIqTF49CcecRNZEh5DIXCB00YL31ozs2";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
